14b726c80bb7700bc1600c122cf96c25ca74e1c8
Nic Dillon <46064401+nicdillon@users.noreply.github.com>
2019-07-08 12:04:07 -0400

    Add files via upload

---
+++ general.js
@@ -0,0 +1,9 @@
+﻿storage = window.localStorage;
+function updateArea(area) {
+    var currentArea = "Area" + area;
+    storage.setItem(currentArea, "Yes");
+}
+
+function clearStorage() {
+    localStorage.clear();
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+var area1Complete = "No";
+var area2Complete = "No";
+
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+
+    function updateLink() {
+
+        document.getElementById('arealink') = 'area' + document.getElementById('areaselect').value + '.html';
+    };
+
+    
+})();
+
+function updateArea1() {
+    area1Complete = "Yes";
+    return area1Complete;
+};
